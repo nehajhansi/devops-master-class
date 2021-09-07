4f800475960521d3ec0271bf641bf898081f8469
nehajhansi <61926628+nehajhansi@users.noreply.github.com>
2021-09-07 23:37:02 +0530

    Update main.tf

--- terraform/backup/01-terraform-basics/main.tf
+++ terraform/backup/01-terraform-basics/main.tf
@@ -1,9 +1,12 @@
-provider "aws" {
+provider "aws" {  #whatever cloud u want to use and when it is executed the required plugins are installed
     region = "us-east-1"
     //version = "~> 2.46" (No longer necessary)
 }
 
 # plan - execute 
+#(terraform plan tells us what terraform will do if u execute that particular code)(and the actions that will be performed)
+#(terraform apply again shows us the plan and if u enter yes then the action will be performed)
+#after terraform apply the terraform.tfstate file is created which is in json
 resource "aws_s3_bucket" "my_s3_bucket" {
     bucket = "my-s3-bucket-in28minutes-rangak-002"
     versioning {
