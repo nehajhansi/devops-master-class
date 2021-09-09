7799bdbd2ae7f9fba33a7106956f0474023b992c
nehajhansi <61926628+nehajhansi@users.noreply.github.com>
2021-09-09 11:15:22 +0530

    Update main.tf

--- terraform/backup/01-terraform-basics/main.tf
+++ terraform/backup/01-terraform-basics/main.tf
@@ -18,6 +18,9 @@ resource "aws_s3_bucket" "my_s3_bucket" {
 # KNOWN STATE- IT IS PRESENT IN THE STATE FILE...It contains the mapping from the terraform name of the resource to the aws identifier of the specific resource and when we perform apply the id in the state file is compared.
 #when the terraform apply is applied then it goes to the refreshing state and compares the present execution with the known execution...if there are no changes then it will deploy in secs
 #terraform is declarative i.e it compares the desired state with actual state and makes it equal
-resource "aws_iam_user" "my_iam_user" {
-    name = "my_iam_user_abc_updated"
+resource "aws_iam_user" "my_iam_user" { #name of the resource in terraform
+    name = "my_iam_user_abc_updated" #name in the cloud
 }
+#you can't store the state file anywhere because it has some confidential information which can't be publically available(so never evr commit them)
+#so direct store it in remote backend i.e in the s3 bucket of the cloud
+# terraform apply and destroy both refresh the states at the beginning
