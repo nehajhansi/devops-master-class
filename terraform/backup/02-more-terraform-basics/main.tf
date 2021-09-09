03183b3cd40b67cdd8ab86e4a6e1245c22ee0a82
nehajhansi <61926628+nehajhansi@users.noreply.github.com>
2021-09-09 12:10:28 +0530

    Update main.tf

--- terraform/backup/02-more-terraform-basics/main.tf
+++ terraform/backup/02-more-terraform-basics/main.tf
@@ -18,4 +18,4 @@ resource "aws_iam_user" "my_iam_users" {
 #terraform console is used to get the values by indexes
 #terraform fmt = used to format
 #terraform show = provides readable format of state file
-#
+#export TF_VAR_iam_user_name_prefix = FROM_ENV_VARIABLE_IAM_PREFIX = override the default value(least priority) and u can do that by tfvars file aswell and from command line aswell use double quotes(higher priority)
